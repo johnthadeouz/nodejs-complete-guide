f8d8257976d3393d37dd400cc56963afc4143812
Jonatan Tadeo <tadeo.calles@outlook.com>
2021-03-15 15:46:50 -0400

    using filesystem to send request with the message to be written into a file in the server, also processing response is required in order to catch the message in the buffer

--- app.js
+++ app.js
@@ -1,11 +1,44 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
+    const url = req.url;
+    const method = req.method;
+    if (url === '/') {
+        res.write(`
+        <html>
+        <head><title>Enter Message</title></head>
+        <body>
+        <form action="/message" method="POST">
+        <input type="text" name="message">
+        <button type="submit">SEND</button>
+        </form>
+        </body>
+        </html>`);
+        return res.end();
+    }
+    if (url === '/message' && method === 'POST') {
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt', message);
+        });
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
     res.setHeader('Content-Type', 'text/html');
-    res.write(`<html>
-    <head><title>My first page</title></title>
-    <body><h1>Hello from my node.js server!</h1></body>
+    res.write(`
+    <html>
+    <head><title>My first page</title></head>
+    <body>
+    <h1>HELLO</h1>
+    </body>
     </html>`);
     res.end();
 });
